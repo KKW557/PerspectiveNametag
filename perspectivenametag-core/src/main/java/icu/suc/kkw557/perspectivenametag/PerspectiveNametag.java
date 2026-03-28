4f22d3724fc300fc276f00e3322b7a1e39c39a85
KKW557 <kevin557557@gmail.com>
2026-03-29 04:59:04 +0800

    core: blacklist

--- perspectivenametag-core/src/main/java/icu/suc/kkw557/perspectivenametag/PerspectiveNametag.java
+++ perspectivenametag-core/src/main/java/icu/suc/kkw557/perspectivenametag/PerspectiveNametag.java
@@ -1,5 +1,9 @@
 package icu.suc.kkw557.perspectivenametag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class PerspectiveNametag {
     public static boolean DISABLED = false;
+    public static final List<String> BLACKLIST = new ArrayList<>();
 }
